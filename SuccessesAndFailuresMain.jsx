58662eba3825c155db4f0deb44a7868e384af559
David Pfahler <pfahler.david@gmail.com>
2019-03-20 08:13:45 +0100

    moved the project to be only a page of the dsa 5 web tools

--- SuccessesAndFailuresMain.jsx
+++ SuccessesAndFailuresMain.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
 
 import DSAInfoBox from '../controls/DSAInfoBox';
 import DSAButton from '../controls/DSAButton';
 import { DSAGrid, DSAGridItem, DSAGridRow} from '../controls/DSAGrid';
 import DSAMediaCard from '../controls/DSAMediaCard';
 import DSADialog from '../controls/DSADialog';
-import DSADescription from '../controls/DSADescription';
 import {mapObject} from '../controls/DSAUtils';
 import {DSARandomAttack} from '../data/DSARandomAttackTables';
+import DSAItemList from '../controls/DSAItemList'
 
 const styles = {
   root: {
@@ -82,7 +83,7 @@ class DSAMain extends React.Component {
   }
 
   getGridItem(item, index){
-    return <DSAGridItem xs={12} md={6} lg={3} key={index}>
+      return <DSAGridItem xs={12} md={6} lg={3} key={index}>
         <DSAMediaCard
           imagesrc={item.image}
           imagetitle={item.imagetitle}
@@ -92,36 +93,35 @@ class DSAMain extends React.Component {
       </DSAGridItem>
   }
 
-  getSuccesses() {
-    return SUCCESSES.map((success, index) =>
-      this.getGridItem(success, index)
-    );
-  }
+  getItems(items, title) {
+    const { width } = this.props;
+    if(isWidthUp('sm', width)) {
+      return <DSAGrid>
+        <DSAGridRow>
+          <DSAInfoBox title={title} />
+        </DSAGridRow>
+        <DSAGrid>
+        {items.map((item, index) => this.getGridItem(item, index))}
+        </DSAGrid>
+      </DSAGrid>
+    }
+    else {
+      return <DSAInfoBox title={title}><DSAItemList items={items.map((item) => {
+        return {
+          name: item.title,
+          tooltip: item.description,
+          action: this.handleClickOpen(this.getFlatternTable(item.table))
+        }
+      })} /></DSAInfoBox>
+    }
 
-  getFails() {
-    return FAILS.map((fails, index) =>
-      this.getGridItem(fails, index)
-    );
   }
 
   render() {
     const { classes } = this.props;
-    return (<div>
-      <main className={classes.root}>
-        <DSAGrid>
-          <DSAGridRow>
-            <DSAInfoBox title="Kritische Erfolge" />
-          </DSAGridRow>
-          <DSAGrid>
-            {this.getSuccesses()}
-          </DSAGrid>
-          <DSAGridRow>
-            <DSAInfoBox title="Kritische Fehlschläge" />
-          </DSAGridRow>
-          <DSAGrid>
-            {this.getFails()}
-          </DSAGrid>
-        </DSAGrid>
+    return <main className={classes.root}>
+            {this.getItems(SUCCESSES, "Kritische Erfolge")}
+            {this.getItems(FAILS, "Kritische Fehlschläge")}
         <DSADialog
           handleClose={this.handleClose}
           open={this.state.open}
@@ -129,26 +129,7 @@ class DSAMain extends React.Component {
           title={this.state.current.title}>
           {this.state.current.description}
         </DSADialog>
-      </main>
-      <footer>
-        <DSAGrid>
-          <DSAGridRow>
-            <DSAInfoBox>
-              <DSADescription caption="Impressum">
-                Website von David Pfahler.
-                Dieses Produkt wurde unter Lizenz erstellt. Das Schwarze Auge und sein Logo sowie Aventuria, Dere, Myranor, Riesland, Tharun und Uthuria
-                und ihre Logos sind eingetragene Marken von Significant GbR in Deutschland, den U.S.A. und anderen Ländern. Ulisses Spiele und sein Logo
-                sind eingetragene Marken der Ulisses Medien und Spiele Distribution GmbH.
-                Dieses Werk enthält Material, das durch Ulisses Spiele und/oder andere Autoren urheberrechtlich geschützt ist. Solches Material wird mit
-                Erlaubnis im Rahmen der Vereinbarung über Gemeinschaftsinhalte für SCRIPTORIUM AVENTURIS verwendet.
-                Alle anderen Originalmaterialien in diesem Werk sind Copyright 2018 von Moritz Schmid und werden im Rahmen der Vereinbarung über
-                Gemeinschaftsinhalte für SCRIPTORIUM AVENTURIS veröffentlicht.
-              </DSADescription>
-            </DSAInfoBox>
-          </DSAGridRow>
-        </DSAGrid>
-      </footer>
-    </div>);
+      </main>;
   }
 };
 
@@ -156,4 +137,4 @@ DSAMain.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(DSAMain);
+export default withWidth()(withStyles(styles)(DSAMain));
